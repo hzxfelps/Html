547d7bf8b24ef265e3f15b0e445d1bb2aedfbc5a
hzxfelps <felipe.augusto.gonzales1a@gmail.com>
2026-03-26 08:43:26 -0300

    Site Para venda de curso do tailwind

---
+++ vendaDeCursoTailwind/app.js
@@ -0,0 +1,10 @@
+//painel do caralho
+function toggleAulas() {
+    const box = document.getElementById('aulasBox');
+    box.classList.toggle('hidden');
+}
+
+function toggle(id) {
+    const el = document.getElementById(id);
+    el.classList.toggle('hidden');
+}
